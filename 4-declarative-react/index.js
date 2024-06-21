edb72b0a4e88ece821dab82f2161b67fe63f12bb
denuwan55 <ldweerarathne5555@gmail.com>
2024-06-21 19:13:07 +0530

    Understood how and what is the need of react property declarivity

---
+++ 4-declarative-react/index.js
@@ -0,0 +1,4 @@
+const para = document.createElement("p");
+para.textContent = "This is the imperative way of doing things";
+para.className = "header";
+document.querySelector("#root").append(para);
