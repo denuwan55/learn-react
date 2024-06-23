24c81475c3bb9afdaf0d82555a467b04f5a1eb39
denuwan55 <ldweerarathne5555@gmail.com>
2024-06-23 17:39:46 +0530

    Learned organising components into their own files

--- 9-organising-react/index.js
+++ 9-organising-react/index.js
@@ -1,25 +1,7 @@
 // index.js
 import Header from "./Header"
-
-function Footer() {
-    return (
-        <footer>
-            <small> 2024 Denuwan development. All rights reserved.</small>
-        </footer>
-    )
-}
-
-function MainContent() {
-    return (
-        <div>
-            <h1>Reasons I'm excited to learn React</h1>
-            <ol>
-                <li>It's a popular library, so I'll be able to fit in with the cool kids!</li>
-                <li>I'm more likely to get a job as a developer if I know React</li>
-            </ol>
-        </div>
-    )
-}
+import Footer from "./Footer"
+import MainContent from "./MainContent"
 
 function Page() {
     return (
