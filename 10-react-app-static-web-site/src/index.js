dfd3b2b37892a17d4d067d8082c3a078e63211cb
denuwan55 <ldweerarathne5555@gmail.com>
2024-06-25 19:58:46 +0530

    App.js

--- 10-react-app-static-web-site/src/index.js
+++ 10-react-app-static-web-site/src/index.js
@@ -1,22 +1,10 @@
 // index.js
-import Header from "./components/Header"
-import Footer from "./components/Footer"
-import MainContent from "./components/MainContent"
 import React from "react"
 import ReactDOM from "react-dom"
 import "./style.css"
-
-function Page() {
-    return (
-        <div>
-            <Header />
-            <MainContent />
-            <Footer />
-        </div>
-    )
-}
+import App from "./App"
 
 ReactDOM.render(
-    <Page />
+    <App />
     , document.querySelector("#root")
 )
